8470b3d16a358af6b755a27905c28f0a60514549
Ubuntu <ubuntu@ip-172-31-93-228.ec2.internal>
2019-08-16 19:30:19 +0000

    connecting to mondo

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'route_reminders'
+app.config["MONGO_URI"] = 'mongodb+srv://bandyp:Ema1LandreW@myfirstcluster-ehsli.mongodb.net/route_reminders?retryWrites=true&w=majority'
 
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_incidents')
+def get_incidents():
+    return render_template("incidents.html", incidents=mongo.db.incidents.find())
 
 
 if __name__ == '__main__':
