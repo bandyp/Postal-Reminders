80808b70a394cdaa138e3dd35a8b8d61e9b5b2a9
Ubuntu <ubuntu@ip-172-31-93-228.ec2.internal>
2019-10-24 20:04:05 +0000

    fixed css to login page

--- app.py
+++ app.py
@@ -110,4 +110,4 @@ def see_login():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=False)
+            debug=os.environ.get('debug'))
